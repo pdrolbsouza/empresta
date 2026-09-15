a947e12ce405b7224004b4aab60a90b83191d800
Pedro <pedro.luis.borges.o@gmail.com>
2026-09-15 08:43:34 -0300

    LoginTest

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function test_login(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->clickLink('Faça login usando senha única USP!')
+                ->waitFor('#loginUsuario')
+                ->type('#loginUsuario', '1111')
+                ->press('Login')
+                ->assertSee('Administração')
+                ->pause('3000')
+                ->click('.login_logout_link')
+                ->assertSee('Sistema de Empréstimo');
+        });
+    }
+}
